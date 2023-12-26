b2dc930b855d9dcdf6bad7d01e53cab44d590b9c
ScrolliStudio <152308836+ScrolliStudio@users.noreply.github.com>
2023-12-26 19:01:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    style: 'mapbox://styles/mapbox/standard',
-    accessToken: 'pk.eyJ1Ijoic2Nyb2xsaSIsImEiOiJjbHEybGFqOGIwMzF2MmpxdWJxaHFod25sIn0.2QPwdF-SfEf-hz1g-U6GSw',
+    style: 'mapbox://styles/scrolli/clqmiw36s00q601o3fmrl4czl',
+    accessToken: 'pk.eyJ1Ijoic2Nyb2xsaSIsImEiOiJjbHE0NjRyMjAwMXF2Mm1udmVuajcxZXMzIn0.7B_-qv4yez-sXAhdGrQ0wA',
     showMarkers: false,
     theme: 'light',
     title: '',
